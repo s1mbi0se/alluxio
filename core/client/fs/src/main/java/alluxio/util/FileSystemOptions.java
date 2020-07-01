f75302d4cf5f07584ef2da80b452bf17884743b0
Abner Ferreira <abner.ferreira@simbioseventures.com>
2020-07-01 09:26:55 -0300

    feat: add documentation for 6 methods

--- core/client/fs/src/main/java/alluxio/util/FileSystemOptions.java
+++ core/client/fs/src/main/java/alluxio/util/FileSystemOptions.java
@@ -187,8 +187,14 @@ public class FileSystemOptions {
   }
 
   /**
-   * @param conf Alluxio configuration
-   * @return options based on the configuration
+   * Builds and returns a new set of options for the file system.
+   * <p>
+   * Utilizes {@link MountPOptions.Builder} in order to create
+   * these options, which are used to set default
+   * properties and permissions for the file system.
+   *
+   * @param conf  Alluxio configuration
+   * @return      options based on the configuration
    */
   public static MountPOptions mountDefaults(AlluxioConfiguration conf) {
     return MountPOptions.newBuilder()
