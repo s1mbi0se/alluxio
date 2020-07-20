03e0cf6996e18a30b1e9219b1e09221147ed3fa0
Abner Eduardo Ferreira <abner.ferreira@simbioseventures.com>
2020-07-20 19:31:25 -0300

    feat: add doc for alluxio.master.file.DefaultFileSystemMaster.removeBlocks(List<Long>)

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -4353,6 +4353,13 @@ public final class DefaultFileSystemMaster extends CoreMaster
         blocks -> blocks.forEach(mUfsBlockLocationCache::invalidate));
   }
 
+  /**
+   * Removes the provided blocks from the block master.
+   *
+   * @param blocks the list of blocks to be removed from the
+   *               {@link alluxio.worker.Worker}s by the
+   *               {@link #mBlockMaster}
+   */
   private void removeBlocks(List<Long> blocks) throws IOException {
     if (blocks.isEmpty()) {
       return;
