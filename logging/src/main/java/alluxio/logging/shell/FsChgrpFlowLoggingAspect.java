a007a7ebf9a53a07a76e7a997135bd2736bc4f4d
Abner Eduardo Ferreira <abner.ferreira@simbioseventures.com>
2020-07-27 13:05:08 -0300

    feat: add logging aspect for file system command, "chgrp"

---
+++ logging/src/main/java/alluxio/logging/shell/FsChgrpFlowLoggingAspect.java
@@ -0,0 +1,41 @@
+package alluxio.logging.shell;
+
+import alluxio.logging.shell.base.FsFlowLoggingAspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * This class represents a {@link alluxio.logging.base.FlowLoggingAspect} for the
+ * file system command {@code chgrp}. All methods triggered by this command are logged
+ * and kept track of.
+ */
+@Aspect
+public final class FsChgrpFlowLoggingAspect extends FsFlowLoggingAspect {
+
+    private static final String FLOW_NAME = "FsChgrpFlow";
+    private static final String START_METHOD = "execution(* alluxio.cli.fs.command.ChgrpCommand.run(..))";
+
+    @Around(START_METHOD)
+    @Override
+    public Object startFlow(final ProceedingJoinPoint point) throws Throwable {
+        return super.startFlow(point);
+    }
+
+    @Around(WHITE_AND_BLACK_LIST)
+    @Override
+    public Object around(final ProceedingJoinPoint point) throws Throwable {
+        return super.around(point);
+    }
+
+    @Around(FINISH_METHOD)
+    @Override
+    public Object finishFlow(final ProceedingJoinPoint point) throws Throwable {
+        return super.finishFlow(point);
+    }
+
+    @Override
+    protected String getFlowName() {
+        return FLOW_NAME;
+    }
+}
