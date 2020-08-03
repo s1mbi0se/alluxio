d41347906c4dca6ed1071f82de17ce77de504c49
Abner Eduardo Ferreira <abner.ferreira@simbioseventures.com>
2020-08-03 16:14:31 -0300

    feat: add doc for alluxio.grpc.GrpcChannelKey.setNetworkGroup(GrpcNetworkGroup)

--- core/common/src/main/java/alluxio/grpc/GrpcChannelKey.java
+++ core/common/src/main/java/alluxio/grpc/GrpcChannelKey.java
@@ -102,7 +102,7 @@ public class GrpcChannelKey {
 
   /**
    * @param group the networking group membership
-   * @return the modified {@link GrpcChannelKey}
+   * @return this gRPC channel key updated under a new group
    */
   public GrpcChannelKey setNetworkGroup(GrpcNetworkGroup group) {
     mNetworkGroup = group;
